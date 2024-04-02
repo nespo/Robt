a75f79edf91a6379b7952b906165cd00d8acd4bd
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-02 19:46:03 +0300

    fix

--- robot_code/code/motor_control.py
+++ robot_code/code/motor_control.py
@@ -5,6 +5,7 @@ parent_dir = os.path.join(script_dir, '..', '..')
 sys.path.append(os.path.abspath(parent_dir))
 from config import config
 from robot_code.modules.motor import Motor
+from robot_code.modules.speed import Speed
 import RPi.GPIO as GPIO
 
 class Robot:
@@ -25,34 +26,60 @@ class Robot:
                                 reverse=config["motors"]["right_rear"]["reverse"]),
         }
 
+        self.left_rear_speed = Speed(25)
+        self.right_rear_speed = Speed(4)  
+
+    def start_speed_thread(self):
+        # left_front_speed.start()
+        # right_front_speed.start()
+        self.left_rear_speed.start()
+        self.right_rear_speed.start()
+
+    def set_motor_power(self, motor, power):
+        if motor == 1:
+            self.motors["left_front"].set_power(power)
+        elif motor == 2:
+            self.motors["right_front"].set_power
+        elif motor == 3:
+            self.motors["left_rear"].set_power
+        elif motor == 4:
+            self.motors["right_rear"].set_power
+    '''      
     def set_all_motor_power(self, left_power, right_power):
         """Set power for all motors for coordinated movement."""
         self.motors["left_front"].set_power(left_power)
         self.motors["right_front"].set_power(right_power)
         self.motors["left_rear"].set_power(left_power)
         self.motors["right_rear"].set_power(right_power)
+    '''
 
-    def forward(self, speed=100):
-        """Move the car forward."""
-        self.set_all_motor_power(speed, speed)
+    def forward(self, power):
+        self.motors["left_front"].set_power(power)
+        self.motors["left_rear"].set_power(power)
+        self.motors["right_front"].set_power(power)
+        self.motors["right_rear"].set_power(power)
 
-    def backward(self, speed=100):
-        """Move the car backward."""
-        self.set_all_motor_power(-speed, -speed)
+    def backward(self, power):
+        self.motors["left_front"].set_power(-power)
+        self.motors["left_rear"].set_power(-power)
+        self.motors["right_front"].set_power(-power)
+        self.motors["right_rear"].set_power(-power)
+
+    def turn_left(self, power):
+        self.motors["left_front"].set_power(-power)
+        self.motors["left_rear"].set_power(-power)
+        self.motors["right_front"].set_power(power)
+        self.motors["right_rear"].set_power(power)
+
+    def turn_right(self, power):
+        self.motors["left_front"].set_power(power)
+        self.motors["left_rear"].set_power(power)
+        self.motors["right_front"].set_power(-power)
+        self.motors["right_rear"].set_power(-power)
 
     def stop(self):
-        """Stop the car."""
-        self.set_all_motor_power(0, 0)
-
-    def turn_left(self, speed=100, sharpness=0.5):
-        """Turn the car left with adjustable sharpness."""
-        left_speed = speed * (1 - sharpness)
-        right_speed = speed
-        self.set_all_motor_power(left_speed, right_speed)
-
-    def turn_right(self, speed=100, sharpness=0.5):
-        """Turn the car right with adjustable sharpness."""
-        right_speed = speed * (1 - sharpness)
-        left_speed = speed
-        self.set_all_motor_power(left_speed, right_speed)
+        self.motors["left_front"].set_power(0)
+        self.motors["left_rear"].set_power(0)
+        self.motors["right_front"].set_power(0)
+        self.motors["right_rear"].set_power(0)
 
