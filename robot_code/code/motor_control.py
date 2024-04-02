9d33f0209c180cf748b26f4c98acbdbc28a34835
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-02 13:46:47 +0300

    Update some code

--- robot_code/code/motor_control.py
+++ robot_code/code/motor_control.py
@@ -1,9 +1,11 @@
-# motor_control.py
 import os, sys
+
 # Adjust the sys.path to include the parent directory of robot_code
 script_dir = os.path.dirname(__file__)
 parent_dir = os.path.join(script_dir, '..', '..')
 sys.path.append(os.path.abspath(parent_dir))
+
+
 from config import config
 from robot_code.modules.motor import Motor
 import RPi.GPIO as GPIO
@@ -27,37 +29,39 @@ class Robot:
                                 reverse=config["motors"]["right_rear"]["reverse"]),
         }
 
-    def apply_motor_power(self, power, reverse=False):
-        for key, motor in self.motors.items():
-            direction_power = -power if (motor.is_reversed ^ reverse) else power
-            motor.set_power(direction_power)
+    def apply_motor_power(self, power, reverse=False, gradual=False):
+        target_power = -power if reverse else power
+        if gradual:
+            for step in range(0, target_power, 10 if target_power > 0 else -10):
+                for key, motor in self.motors.items():
+                    motor.set_power(step if not motor.is_reversed else -step)
+                time.sleep(0.05)  # Adjust the sleep time for smoother acceleration
+        else:
+            for key, motor in self.motors.items():
+                motor.set_power(target_power if not motor.is_reversed else -target_power)
 
-    def forward(self, power=100):
-        self.apply_motor_power(power)
+    def forward(self, power=100, gradual=False):
+        self.apply_motor_power(power, gradual=gradual)
 
-    def backward(self, power=100):
-        self.apply_motor_power(power, reverse=True)
+    def backward(self, power=100, gradual=False):
+        self.apply_motor_power(power, reverse=True, gradual=gradual)
 
-    def turn_left(self, power=100, pivot=False):
-        if pivot:  # Pivot turn
-            self.motors["left_front"].set_power(-power)
-            self.motors["left_rear"].set_power(-power)
-        else:  # Smooth turn
-            self.motors["left_front"].set_power(power // 2)
-            self.motors["left_rear"].set_power(power // 2)
-        self.motors["right_front"].set_power(power)
-        self.motors["right_rear"].set_power(power)
+    def turn(self, direction, power=100, radius=0):
+        if direction == "left":
+            left_power = max(0, power - radius)
+            right_power = power
+        else:  # direction == "right"
+            left_power = power
+            right_power = max(0, power - radius)
+        
+        self.motors["left_front"].set_power(left_power)
+        self.motors["left_rear"].set_power(left_power)
+        self.motors["right_front"].set_power(right_power)
+        self.motors["right_rear"].set_power(right_power)
 
-    def turn_right(self, power=100, pivot=False):
-        self.motors["left_front"].set_power(power)
-        self.motors["left_rear"].set_power(power)
-        if pivot:  # Pivot turn
-            self.motors["right_front"].set_power(-power)
-            self.motors["right_rear"].set_power(-power)
-        else:  # Smooth turn
-            self.motors["right_front"].set_power(power // 2)
-            self.motors["right_rear"].set_power(power // 2)
-
-    def stop(self):
-        for motor in self.motors.values():
-            motor.set_power(0)
+    def stop(self, gradual=False):
+        if gradual:
+            self.apply_motor_power(0, gradual=True)
+        else:
+            for motor in self.motors.values():
+                motor.set_power(0)
