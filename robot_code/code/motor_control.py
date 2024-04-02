1d2965c64c3ad91e917ae6af40bd82cb9e640147
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-02 14:21:48 +0300

    fix

--- robot_code/code/motor_control.py
+++ robot_code/code/motor_control.py
@@ -11,37 +11,48 @@ class Robot:
     def __init__(self, config):
         GPIO.setmode(GPIO.BCM)
         self.motors = {
-            "left": Motor(pin_pwm=config["motors"]["left_front"]["pin_pwm"],
-                          pin_dir=config["motors"]["left_front"]["pin_dir"],
-                          reverse=config["motors"]["left_front"]["reverse"]),
-            "right": Motor(pin_pwm=config["motors"]["right_front"]["pin_pwm"],
-                           pin_dir=config["motors"]["right_front"]["pin_dir"],
-                           reverse=config["motors"]["right_front"]["reverse"])
+            "left_front": Motor(pin_pwm=config["motors"]["left_front"]["pin_pwm"],
+                                pin_dir=config["motors"]["left_front"]["pin_dir"],
+                                reverse=config["motors"]["left_front"]["reverse"]),
+            "right_front": Motor(pin_pwm=config["motors"]["right_front"]["pin_pwm"],
+                                 pin_dir=config["motors"]["right_front"]["pin_dir"],
+                                 reverse=config["motors"]["right_front"]["reverse"]),
+            "left_rear": Motor(pin_pwm=config["motors"]["left_rear"]["pin_pwm"],
+                               pin_dir=config["motors"]["left_rear"]["pin_dir"],
+                               reverse=config["motors"]["left_rear"]["reverse"]),
+            "right_rear": Motor(pin_pwm=config["motors"]["right_rear"]["pin_pwm"],
+                                pin_dir=config["motors"]["right_rear"]["pin_dir"],
+                                reverse=config["motors"]["right_rear"]["reverse"]),
         }
-        # Simplify by using just front motors for direction and speed control
 
-    def set_motor_power(self, left_power, right_power):
-        self.motors["left"].set_power(left_power)
-        self.motors["right"].set_power(right_power)
+    def set_all_motor_power(self, left_power, right_power):
+        """Set power for all motors for coordinated movement."""
+        self.motors["left_front"].set_power(left_power)
+        self.motors["right_front"].set_power(right_power)
+        self.motors["left_rear"].set_power(left_power)
+        self.motors["right_rear"].set_power(right_power)
 
     def forward(self, speed=100):
-        self.set_motor_power(speed, speed)
+        """Move the car forward."""
+        self.set_all_motor_power(speed, speed)
 
     def backward(self, speed=100):
-        self.set_motor_power(-speed, -speed)
+        """Move the car backward."""
+        self.set_all_motor_power(-speed, -speed)
 
     def stop(self):
-        self.set_motor_power(0, 0)
+        """Stop the car."""
+        self.set_all_motor_power(0, 0)
 
-    def turn_left(self, speed=100, pivot=False):
-        if pivot:
-            self.set_motor_power(-speed, speed)
-        else:
-            self.set_motor_power(speed * 0.5, speed)
+    def turn_left(self, speed=100, sharpness=0.5):
+        """Turn the car left with adjustable sharpness."""
+        left_speed = speed * (1 - sharpness)
+        right_speed = speed
+        self.set_all_motor_power(left_speed, right_speed)
 
-    def turn_right(self, speed=100, pivot=False):
-        if pivot:
-            self.set_motor_power(speed, -speed)
-        else:
-            self.set_motor_power(speed, speed * 0.5)
+    def turn_right(self, speed=100, sharpness=0.5):
+        """Turn the car right with adjustable sharpness."""
+        right_speed = speed * (1 - sharpness)
+        left_speed = speed
+        self.set_all_motor_power(left_speed, right_speed)
 
