220f690b4b292ad9f71984e2a4cdf5c6f1791703
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-02 20:22:19 +0300

    fix

--- robot_code/code/motor_control.py
+++ robot_code/code/motor_control.py
@@ -56,10 +56,10 @@ class Robot:
     
     '''
     def forward(self, power):
-        self.motors["left_front"].set_power(power)
+        self.motors["left_front"].set_power(power/2)
         self.motors["left_rear"].set_power(power)
         self.motors["right_front"].set_power(power/2)
-        self.motors["right_rear"].set_power(power/2)
+        self.motors["right_rear"].set_power(power)
   
         
     def backward(self, power):
