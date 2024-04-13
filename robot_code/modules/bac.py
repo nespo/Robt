bd5f3da90fb233026ab541f9eb1910bcecbcc6f0
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-13 21:48:13 +0300

    FIX

--- robot_code/modules/bac.py
+++ robot_code/modules/bac.py
@@ -30,16 +30,18 @@ class LidarScanner:
         try:
             for scan in self.lidar.iter_scans():
                 scan_data = {round(measurement[1]): measurement[2] for measurement in scan if measurement[0] > 0}
-                yield scan_data
+                yield scan_data  # Use yield instead of return
         except RPLidarException as e:
             logging.error(f"Lidar scanning error: {e}")
             self.close()
             raise
 
+
     def close(self):
         self.lidar.stop()
         self.lidar.disconnect()
         logging.info("LIDAR disconnected safely.")
+
 
 class ObstacleChecker:
     def __init__(self, lidar, us, config):
@@ -51,11 +53,11 @@ class ObstacleChecker:
         self.us_data = {}
 
     def get_lidar_data(self):
+        # Assume you want to use the most recent scan only
         try:
-            self.lidar_data = next(self.lidar.iter_scans(), {})
-            logging.debug(f"Lidar Data: {self.lidar_data}")
+            self.lidar_data = next(self.lidar.iter_scans())
         except StopIteration:
-            logging.error("No more LIDAR scans available.")
+            # No more scans available
             self.lidar_data = {}
         except RPLidarException as e:
             logging.error(f"Error obtaining LIDAR data: {e}")
@@ -74,24 +76,21 @@ class ObstacleChecker:
 
     def merge_sensor_data(self):
         sensor_data = np.full(360, self.config['max_distance'])
+        # Make sure lidar_data is a dictionary before attempting to iterate over its items
         if isinstance(self.lidar_data, dict):
             for angle, distance in self.lidar_data.items():
-                adjusted_angle = angle % 360
-                sensor_data[adjusted_angle] = distance
+                sensor_data[angle % 360] = distance
 
-        if isinstance(self.us_data, dict):
-            for angle, distance in self.us_data.items():
-                adjusted_angle = angle % 360
-                sensor_data[adjusted_angle] = min(sensor_data[adjusted_angle], distance)
+        # Assuming us_data is also a dictionary
+        for angle, distance in self.us_data.items():
+            adjusted_angle = angle % 360
+            sensor_data[adjusted_angle] = min(sensor_data[adjusted_angle], distance)
 
-        logging.debug(f"Merged Sensor Data: {sensor_data}")
         return sensor_data
 
     def check_for_obstacles(self):
         self.start_ultrasonic_sweep()
         self.get_lidar_data()
         if self.us_thread is not None:
-            self.us_thread.join()
-        sensor_data = self.merge_sensor_data()
-        logging.debug(f"Obstacle Check: {sensor_data}")
-        return sensor_data
+            self.us_thread.join()  # Ensure ultrasonic data collection is complete
+        return self.merge_sensor_data()
