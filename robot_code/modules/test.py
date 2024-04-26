b8ce99b08256be5a706b6007d26d465740e24c92
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-26 16:17:50 +0300

    fix

--- robot_code/modules/test.py
+++ robot_code/modules/test.py
@@ -4,14 +4,15 @@ import time
 # Setup serial connection
 ser = serial.Serial('/dev/ttyUSB0', 256000, timeout=1)
 
-# Attempt to read data
 try:
     print("Starting to read from serial port...")
     time.sleep(2)  # Give it a moment before reading
     while True:
-        if ser.in_waiting > 0:
-            data = ser.read(ser.in_waiting)
+        data = ser.read(100)  # Attempt to read 100 bytes
+        if data:
             print('Received:', data)
+        else:
+            print('No data received.')
         time.sleep(1)
 except KeyboardInterrupt:
     print("Program interrupted by user.")
