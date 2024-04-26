3e45e664028e5e72aeafd8ba7c5eaaaa7a615948
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-26 15:59:53 +0300

    fix

--- robot_code/modules/test.py
+++ robot_code/modules/test.py
@@ -1,37 +1,17 @@
-import time
 from rplidar import RPLidar
+lidar = RPLidar('/dev/ttyUSB0')
 
-# Define the port name for the RPLIDAR
-PORT_NAME = '/dev/ttyUSB0'  # Change this to your actual port name
+info = lidar.get_info()
+print(info)
 
-# Function to process scan data
-def process_scan_data(scan):
-    for angle, distance in scan:
-        print(f"Angle: {angle}, Distance: {distance}")
+health = lidar.get_health()
+print(health)
 
-# Connect to the RPLIDAR
-lidar = RPLidar(PORT_NAME)
+for i, scan in enumerate(lidar.iter_scans()):
+    print('%d: Got %d measurments' % (i, len(scan)))
+    if i > 10:
+        break
 
-try:
-    # Start scanning
-    lidar.start_motor()
-    lidar.connect()
-    
-    # Start scanning
-    lidar.start_scan()
-    
-    # Get scan data for 30 seconds
-    end_time = time.time() + 30
-    while time.time() < end_time:
-        # Read scan data
-        scan_data = lidar.iter_scans()
-        
-        # Process the scan data
-        for scan in scan_data:
-            process_scan_data(scan)
-        
-finally:
-    # Stop scanning and clean up
-    lidar.stop_scan()
-    lidar.stop_motor()
-    lidar.disconnect()
+lidar.stop()
+lidar.stop_motor()
+lidar.disconnect()
