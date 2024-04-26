29ba0bbddccb26b34499421d46e94f9621096849
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-26 15:15:42 +0300

    FIX:

--- robot_code/modules/global_2.py
+++ robot_code/modules/global_2.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.abspath(parent_dir))
 
 # Import robot-specific modules
 from robot_code.code.motor_control import Robot
-from robot_code.modules.sensor import LidarScanner, ObstacleChecker
+from robot_code.modules.lidar import LidarScanner, ObstacleChecker
 from robot_code.code.config import config
 from robot_code.modules.nav import get_current_gps, get_current_heading
 
