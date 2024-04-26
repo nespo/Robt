67276954cd42dc02117c99c247a53bbd8af9adc1
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-26 16:04:48 +0300

    FIX:

--- robot_code/modules/test.py
+++ robot_code/modules/test.py
@@ -1,26 +1,42 @@
-from pyrplidar import PyRPlidar
+from pyrplidar import PyRPlidar, PyRPlidarException
 
-lidar = PyRPlidar()
-lidar.connect(port="/dev/ttyUSB0", baudrate=256000, timeout=3)
-# Linux   : "/dev/ttyUSB0"
-# MacOS   : "/dev/cu.SLAB_USBtoUART"
-# Windows : "COM5"
+try:
+    lidar = PyRPlidar()
+    
+    # Connect to the Lidar device
+    lidar.connect(port="/dev/ttyUSB0", baudrate=256000, timeout=3)
+    # Linux: "/dev/ttyUSB0"
+    # MacOS: "/dev/cu.SLAB_USBtoUART"
+    # Windows: "COM5"
+    
+    print("PyRPlidar Info: Device connected")
+    
+    try:
+        # Get device information
+        info = lidar.get_info()
+        print("Device Info:", info)
+        
+        # Get device health
+        health = lidar.get_health()
+        print("Device Health:", health)
+        
+        # Get current scan mode
+        scan_modes = lidar.get_scan_modes()
+        print("Scan Modes:")
+        for scan_mode in scan_modes:
+            print(scan_mode)
+        
+        # Get samplerate
+        samplerate = lidar.get_samplerate()
+        print("Samplerate:", samplerate)
+    
+    except PyRPlidarException as e:
+        print("Error:", e)
+    
+    finally:
+        # Disconnect from the Lidar device
+        lidar.disconnect()
+        print("PyRPlidar Info: Device disconnected")
 
-
-info = lidar.get_info()
-print("info :", info)
-
-health = lidar.get_health()
-print("health :", health)
-
-samplerate = lidar.get_samplerate()
-print("samplerate :", samplerate)
-
-
-scan_modes = lidar.get_scan_modes()
-print("scan modes :")
-for scan_mode in scan_modes:
-    print(scan_mode)
-
-
-lidar.disconnect()
+except PyRPlidarException as e:
+    print("Connection Error:", e)
